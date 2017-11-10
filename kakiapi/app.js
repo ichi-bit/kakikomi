5bed920f6ecad2106574722b32de1352421cf142
Munekazu Haranaga <ichi-bit@google.com>
2017-11-10 15:44:03 +0900

    画像アップロードに対応

--- kakiapi/app.js
+++ kakiapi/app.js
@@ -3,30 +3,47 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-/// 追加
-var mongoose = require('mongoose')
-var restify = require('express-restify-mongoose')
-///
+var mongoose = require('mongoose');
+var restify = require('express-restify-mongoose');
 
 var app = express();
-
-/// 追加
 var router = express.Router();
-///
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static('public'));
 
-/// 追加
 mongoose.connect('mongodb://localhost/app1',{useMongoClient:true});
 restify.serve(router, mongoose.model('Post', new mongoose.Schema({
   name: { type: String },
-  kakikomi: { type: String }
+  kakikomi: { type: String },
+  filename: {type: String}
 })));
 app.use(router);
-/// 
+
+/////////////////////////
+///  追加
+/////////////////////////
+var path = require('path');  // ファイルの拡張子を取得するのに使う
+var multer  = require('multer');
+// 格納場所と新しくつけるファイル名の定義
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+  }
+});
+var upload = multer({ storage: storage });
+// ルーティング
+app.post('/api/upload', upload.single('image'), function (req, res, next) {
+  delete req.file.buffer; // responseには入れない
+  res.json(req.file); // 取得した情報を返す
+});
+/////
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,7 +60,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({error:'error'});
 });
 
 module.exports = app;
--- kakifront/src/App.js
+++ kakifront/src/App.js
@@ -1,148 +1,203 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 /**
- * 書き込み１件
- */
+* 画像アップ
+*/
+class ImageUpload extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    var form = new FormData();
+    form.append('image',e.target.files[0]);
+
+    console.log(e.target.files[0]);
+
+    fetch('/api/upload', {
+      method: "POST",
+
+      body: form
+    }).then(
+      response => response.json()
+    ).then(
+      data => {
+      this.props.onChange(data.filename);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="file" name="image" onChange={this.handleChange} />
+      </div>
+    );
+  }
+}
+
+/**
+* 書き込み１件
+*/
 class Post extends Component {
-    render() {
-        return (
-            <div className="kakikomi">
-                <div>{this.props.post.kakikomi}</div>
-                <p className="name">{this.props.post.name}</p>
-            </div>
-        );
+  render() {
+    var image = '';
+    if (this.props.post.filename) {
+      image = (<img src={`http://localhost:3000/uploads/${this.props.post.filename}`} alt={this.props.post.filename} />);
     }
+    return (
+      <div className="kakikomi">
+        <div>{this.props.post.kakikomi}</div>
+        {image}
+        <p className="name">{this.props.post.name}</p>
+      </div>
+    );
+  }
 }
 
 /**
- * 書き込みリスト
- */
+* 書き込みリスト
+*/
 class List extends Component {
-    // 書き込みリスト
-    render() {
-        const posts = this.props.posts;
-        var list = [];
-        for (var i in posts) {
-            list.push( <li key={i}><Post post={posts[i]} /></li> );
-        }
-        return (<ul>{list}</ul>);
+  // 書き込みリスト
+  render() {
+    const posts = this.props.posts;
+    var list = [];
+    for (var i in posts) {
+      list.push( <li key={i}><Post post={posts[i]} /></li> );
     }
+    return (<ul>{list}</ul>);
+  }
 }
 
 /**
- * 投稿フォーム
- */
+* 投稿フォーム
+*/
 class UpForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            kakikomi:'',
-            name:'',
-            message:'書き込んでください'
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        fetch('/api/v1/Post', {
-            method : "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify({
-                kakikomi: this.state.kakikomi,
-                name : this.state.name
-            })
-        }).then(data => {
-            this.setState({
-                message:'アップしました',
-                kakikomi: '',
-                name: ''
-            });
-        }).then(() => {this.props.onSubmit()});
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      kakikomi:'',
+      name:'',
+      message:'書き込んでください',
+      filename: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFile = this.handleFile.bind(this);
+  }
 
-    handleChange = (e) => {
-        this.setState({message:''});
-        this.setState({ [e.target.name] :e.target.value});
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('/api/v1/Post', {
+      method : "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        kakikomi: this.state.kakikomi,
+        name : this.state.name,
+        filename: this.state.filename
+      })
+    }).then(data => {
+      this.setState({
+        message:'アップしました',
+        kakikomi: '',
+        name: '',
+        filename: ''
+      });
+    }).then(() => {this.props.onSubmit()});
+  }
 
-    render() {
-        return  (
-            <div className="form">
-                <p>{this.state.message}</p>
-                <form onSubmit={this.handleSubmit}>
-                  <label>内容</label>
-                  <textarea name="kakikomi" value={this.state.kakikomi} onChange={this.handleChange}></textarea>
-                  <label>名前</label>
-                  <input type="text" name="name" value={this.state.name}  onChange={this.handleChange} />
-                  <button type="submit">投稿</button>
-                </form>
-            </div>
-        );
-    }
+  handleChange = (e) => {
+    this.setState({message:''});
+    this.setState({ [e.target.name] :e.target.value});
+  }
+
+  handleFile = (filename) => {
+    console.log(filename);
+    this.setState({filename : filename});
+  }
+
+  render() {
+    return  (
+      <div className="form">
+        <p>{this.state.message}</p>
+        <form onSubmit={this.handleSubmit}>
+          <label>内容</label>
+          <textarea name="kakikomi" value={this.state.kakikomi} onChange={this.handleChange}></textarea>
+          <label>名前</label>
+          <input type="text" name="name" value={this.state.name}  onChange={this.handleChange} />
+          <button type="submit">投稿</button>
+        </form>
+        <label>画像</label>
+        <ImageUpload onChange={this.handleFile}/>
+      </div>
+    );
+  }
 }
 
 /**
- * ヘッダー
- */
+* ヘッダー
+*/
 class Header extends Component {
-    render() {
-        return (
-            <header>
-                <h1>掲示板</h1>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header>
+      <h1>掲示板</h1>
+      </header>
+    );
+  }
 }
 
 /**
- * 全画面
- */
+* 全画面
+*/
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        };
-        this.updatePosts();
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+    this.updatePosts();
 
-        this.updatePosts = this.updatePosts.bind(this);
-    }
+    this.updatePosts = this.updatePosts.bind(this);
+  }
 
-    updatePosts(e) {
-        getPost((data) => {
-            this.setState({posts: data});
-        });
-    }
+  updatePosts(e) {
+    getPost((data) => {
+      this.setState({posts: data});
+    });
+  }
 
-    render() {
-        // this.updatePosts();
-        return (
-            <div className="App">
-                <Header />
-                <UpForm onSubmit={this.updatePosts}/>
-                <List posts={this.state.posts}/>
-            </div>
-        );
-    }
+  render() {
+    // this.updatePosts();
+    return (
+      <div className="App">
+      <Header />
+      <UpForm onSubmit={this.updatePosts}/>
+      <List posts={this.state.posts}/>
+      </div>
+    );
+  }
 }
 
 /**
- * サーバーから書き込み一覧を取得する
- * @method getPost
- * @param  {Function} callback データ取得後のコールバック
- * @return {[type]}
- */
+* サーバーから書き込み一覧を取得する
+* @method getPost
+* @param  {Function} callback データ取得後のコールバック
+* @return {[type]}
+*/
 function getPost(callback) {
-    fetch('/api/v1/Post?sort={"_id":-1}')
-        .then(response => response.json())
-        .then((data) => {callback(data)});
+  fetch('/api/v1/Post?sort={"_id":-1}')
+  .then(response => response.json())
+  .then((data) => {callback(data)});
 }
 
 export default App;
